609cd9c9aaea813a8bd175be8255f6ed187d7ecd
writebooksonline <89612530+codingtutorials23@users.noreply.github.com>
2021-12-08 18:21:24 -0500

    Update statements.cs

--- statements.cs
+++ statements.cs
@@ -1,3 +1,9 @@
 static class statements{
- 
+ var If = new if;
+ var Else = new else;
+ var Class = new class;
+ var Static = new static;
+ var While = new while;
+ var False = new false;
+ var True = new true;
 }
